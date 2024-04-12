65be887e196dd00cb0d97da160d7d9a858b773dc
Andre Mogrovejo <andre.mogroveo@trade.ec>
2024-04-12 14:20:04 -0500

    Initial Commit

---
+++ ThreadsGamma/ThreadsGammaApp.swift
@@ -0,0 +1,17 @@
+//
+//  ThreadsGammaApp.swift
+//  ThreadsGamma
+//
+//  Created by IMAC ANDRE on 12/04/24.
+//
+
+import SwiftUI
+
+@main
+struct ThreadsGammaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
